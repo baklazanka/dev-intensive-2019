c67a0c046b8606cbdcc3b3e72e83169c72d19eb3
baklazanka <alina-baklazanka@yandex.ru>
2019-07-03 23:40:12 +0200

    hometask_2: transliteration; Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -28,8 +28,108 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    // FIX
-    return ""
+    val diff = date.time - this.time
+    val diffSeconds = diff / SECOND
+    val diffMinutes = diff / MINUTE
+    val diffHours = diff / HOUR
+    val diffDays = diff / DAY
+
+    var result: String = ""
+    when{
+        (diffSeconds >= 0 && diffSeconds <= 1) -> result = "только что"
+        (diffSeconds > 1 && diffSeconds <= 45) -> result = "несколько секунд назад"
+        (diffSeconds > 45 && diffSeconds <= 75) -> result = "минуту назад"
+        (diffSeconds > 75 && diffMinutes <= 45) -> when(diffMinutes.toInt()){
+            11 -> result = "$diffMinutes минут назад"
+            12 -> result = "$diffMinutes минут назад"
+            13 -> result = "$diffMinutes минут назад"
+            14 -> result = "$diffMinutes минут назад"
+            else -> when((diffMinutes).toInt() % 10){
+                1 -> result = "$diffMinutes минуту назад"
+                2 -> result = "$diffMinutes минуты назад"
+                3 -> result = "$diffMinutes минуты назад"
+                4 -> result = "$diffMinutes минуты назад"
+                else -> result = "$diffMinutes минут назад"
+            }
+        }
+        (diffMinutes > 45 && diffMinutes <= 75) -> result = "час назад"
+        (diffMinutes > 75 && diffHours <= 22) -> when(diffHours.toInt()){
+            11 -> result = "$diffHours часов назад"
+            12 -> result = "$diffHours часов назад"
+            13 -> result = "$diffHours часов назад"
+            14 -> result = "$diffHours часов назад"
+            else -> when((diffHours).toInt() % 10){
+                1 -> result = "$diffHours час назад"
+                2 -> result = "$diffHours часа назад"
+                3 -> result = "$diffHours часа назад"
+                4 -> result = "$diffHours часа назад"
+                else -> result = "$diffHours часов назад"
+            }
+        }
+        (diffHours > 22 && diffHours <= 26) -> result = "день назад"
+        (diffHours > 26 && diffDays <= 360) -> when(diffDays.toInt()){
+            11 -> result = "$diffDays дней назад"
+            12 -> result = "$diffDays дней назад"
+            13 -> result = "$diffDays дней назад"
+            14 -> result = "$diffDays дней назад"
+            else -> when((diffDays).toInt() % 10){
+                1 -> result = "$diffDays день назад"
+                2 -> result = "$diffDays дня назад"
+                3 -> result = "$diffDays дня назад"
+                4 -> result = "$diffDays дня назад"
+                else -> result = "$diffDays дней назад"
+            }
+        }
+        (diffDays > 360) -> result = "более года назад"
+
+        (diffSeconds <= 0 && diffSeconds >= -1) -> result = "только что"
+        (diffSeconds < -1 && diffSeconds >= -45) -> result = "через несколько секунд"
+        (diffSeconds < -45 && diffSeconds >= -75) -> result = "через минуту"
+        (diffSeconds < -75 && diffMinutes >= -45) -> when(diffMinutes.toInt()){
+            -11 -> result = "через ${-diffMinutes} минут"
+            -12 -> result = "через ${-diffMinutes} минут"
+            -13 -> result = "через ${-diffMinutes} минут"
+            -14 -> result = "через ${-diffMinutes} минут"
+            else -> when(diffMinutes.toInt() % 10){
+                -1 -> result = "через ${-diffMinutes} минуту"
+                -2 -> result = "через ${-diffMinutes} минуты"
+                -3 -> result = "через ${-diffMinutes} минуты"
+                -4 -> result = "через ${-diffMinutes} минуты"
+                else -> result = "через ${-diffMinutes} минут"
+            }
+        }
+        (diffMinutes < -45 && diffMinutes >= -75) -> result = "час назад"
+        (diffMinutes < -75 && diffHours >= -22) -> when(diffHours.toInt()){
+            -11 -> result = "через ${-diffHours} часов"
+            -12 -> result = "через ${-diffHours} часов"
+            -13 -> result = "через ${-diffHours} часов"
+            -14 -> result = "через ${-diffHours} часов"
+            else -> when(diffHours.toInt() % 10){
+                -1 -> result = "через ${-diffHours} час"
+                -2 -> result = "через ${-diffHours} часа"
+                -3 -> result = "через ${-diffHours} часа"
+                -4 -> result = "через ${-diffHours} часа"
+                else -> result = "через ${-diffHours} часов"
+            }
+        }
+        (diffHours < -22 && diffHours >= -26) -> result = "день назад"
+        (diffHours < -26 && diffDays >= -360) -> when(diffDays.toInt()){
+            -11 -> result = "через ${-diffDays} дней"
+            -12 -> result = "через ${-diffDays} дней"
+            -13 -> result = "через ${-diffDays} дней"
+            -14 -> result = "через ${-diffDays} дней"
+            else -> when(diffDays.toInt() % 10){
+                -1 -> result = "через ${-diffDays} день"
+                -2 -> result = "через ${-diffDays} дня"
+                -3 -> result = "через ${-diffDays} дня"
+                -4 -> result = "через ${-diffDays} дня"
+                else -> result = "через ${-diffDays} дней"
+            }
+        }
+        (diffDays < -360) -> result = "более чем через год"
+    }
+
+    return result
 }
 
 enum class TimeUnits{
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -15,10 +15,6 @@ object Utils {
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        return ""
-    }
-
     fun toInitials(firstName: String?, lastName: String?): String? {
         if(firstName.isNullOrBlank() && lastName.isNullOrBlank()){
             return null
@@ -30,5 +26,55 @@ object Utils {
         else{
             return (firstName?.substring(0, 1) + lastName?.substring(0, 1)).toUpperCase()
         }
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val transliterationMap: Map<Char, String> = mapOf(
+            'а' to "a",  'А' to "A",
+            'б' to "b",  'Б' to "B",
+            'в' to "v",  'В' to "V",
+            'г' to "g",  'Г' to "G",
+            'д' to "d",  'Д' to "D",
+            'е' to "e",  'Е' to "E",
+            'ё' to "e",  'Ё' to "E",
+            'ж' to "zh", 'Ж' to "Zh",
+            'з' to "z",  'З' to "Z",
+            'и' to "i",  'И' to "I",
+            'й' to "i",  'Й' to "I",
+            'к' to "k",  'К' to "K",
+            'л' to "l",  'Л' to "L",
+            'м' to "m",  'М' to "M",
+            'н' to "n",  'Н' to "N",
+            'о' to "o",  'О' to "O",
+            'п' to "p",  'П' to "P",
+            'р' to "r",  'Р' to "R",
+            'с' to "s",  'С' to "S",
+            'т' to "t",  'Т' to "T",
+            'у' to "u",  'У' to "U",
+            'ф' to "f",  'Ф' to "F",
+            'х' to "h",  'Х' to "H",
+            'ц' to "c",  'Ц' to "C",
+            'ч' to "ch", 'Ч' to "Ch",
+            'ш' to "sh", 'Ш' to "Sh",
+            'щ' to "sh'",'Щ' to "Sh'",
+            'ъ' to "",   'Ъ' to "",
+            'ы' to "i",  'Ы' to "I",
+            'ь' to "",   'Ь' to "",
+            'э' to "e",  'Э' to "E",
+            'ю' to "yu", 'Ю' to "Yu",
+            'я' to "ya", 'Я' to "Ya",
+            ' ' to divider)
+
+        var resultString: String = ""
+
+        for (letter in payload){
+            if(!transliterationMap.containsKey(letter)){
+                resultString += letter.toString()
+                continue
+            }
+            resultString += transliterationMap[letter]
+        }
+
+        return resultString
     }
 }
